6338c0efae071094a2483644bdae89e4fc214428
elfinsanjaya12 <elfinsanjaya1207@gmail.com>
2018-12-17 12:37:16 -0800

    week1 Mengenal String lebih lanjut

---
+++ week1/2_mengenal_String_lebih_lanjut.js
@@ -0,0 +1,26 @@
+// var string = 'Belajar Javasctipt'
+// var jumlah = string.length
+
+// menampilkan Sting baris pertama
+// console.log(string[0])
+
+// menghitung total karakter string
+// console.log(jumlah)
+
+// mengubah semua karakter string menjadi kapital
+// console.log(string.toUpperCase())
+
+//  mengubah semua karakter string menjadi kecil
+// console.log(string.toLowerCase())
+
+// ===============================================================
+// string typecasting  / string number / number string
+
+var stringAngka = '13'
+// convert string ke number
+var ubahKeNumber = Number(stringAngka)
+var hasil = ubahKeNumber + 1
+
+console.log(hasil)
+// convert number ke string
+console.log(String(hasil) + 1)
