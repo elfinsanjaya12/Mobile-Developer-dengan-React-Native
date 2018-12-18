107cf704fc39fd3fbf330763c210480c9d05d941
elfinsanjaya12 <elfinsanjaya1207@gmail.com>
2018-12-17 19:44:09 -0800

    week1 conditional javacript

---
+++ week1/1_conditional_javascript.js
@@ -0,0 +1,39 @@
+// menggunakan operator === sama dengan
+// var kondisi = 'setengah kenyang'
+// if(kondisi === 'lapar'){
+//     console.log("makan")
+// }else if (kondisi === 'setengah kenyang') {
+//     console.log("minta tambah lagi")
+// }else {
+//     console.log("lanjut belajar lagi")
+// }
+
+// menggunkan >= <= 
+// var nilai1 = 100
+// var nilai2 = 60
+
+// if(nilai1 >= 80 && nilai1 >= 80) {
+//     console.log("mendapat hadiah")
+// }else {
+//     console.log("remedial")
+// }
+
+// switch 
+
+var waktu = 'pagi'
+
+switch(waktu){
+    case 'pagi':
+        console.log('selamat pagi')
+    break;
+    case 'siang':
+        console.log('selamat siang')
+    break;
+    case 'sore':
+        console.log('selamat sore')
+    break;
+    default:
+        console.log('selamat berjuang')
+}
+
+
