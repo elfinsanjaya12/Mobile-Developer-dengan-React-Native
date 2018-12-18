f3cfb6e3e70a12c6bd556228fe972308b955bc8a
elfinsanjaya12 <elfinsanjaya1207@gmail.com>
2018-12-18 07:50:04 -0800

    week1 looping

---
+++ week1/looping.js
@@ -0,0 +1,16 @@
+for(var i = 0; i <= 100; i++){
+    console.log(i)
+}
+
+var jumlahMakan = 0
+
+while(jumlahMakan <= 20) {
+    jumlahMakan = jumlahMakan + 1
+    console.log("makan")
+}
+
+var array = ['ini', 'elfin', 'ganteng']
+
+for(var i = 0; i < array.length; i++){
+    console.log(array[i])
+}
