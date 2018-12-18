61106a669054cbbb0cbb139948bd91acdda128ba
elfinsanjaya12 <elfinsanjaya1207@gmail.com>
2018-12-18 08:31:15 -0800

    week1 looping challenges

---
+++ week1/looping_challenges.js
@@ -0,0 +1,72 @@
+// 1. Melakukan Looping Menggunakan While
+// var naik = 1
+// console.log('LOOPING PERTAMA')
+// while(naik < 20){
+//     naik = naik + 1
+//     console.log(naik + '- I love coding')
+// }
+
+// var turun = 20
+// console.log('LOOPING KEDUA')
+// while(turun > 2){
+//     turun = turun - 1
+//     console.log(turun + '- I will become  developer')
+// }
+
+// 2. Melakukan Looping Menggunakan For
+// console.log('LOOPING PERTAMA')
+// for(var i = 1; i <= 20; i++){
+//     console.log(i + '- I love coding')
+// }
+
+// console.log('LOOPING KEDUA')
+// for(var i = 20; i >= 1; i--){
+//     console.log(i + '- I will become  developer')
+// }
+
+
+// 3. Angka Ganjil dan Genap
+// for(var counter = 1; counter <= 100; counter++){
+//     if (counter % 2 === 0) {
+//         console.log('counter sekarang = ' + counter)
+//         console.log('GENAP')
+//     } else {
+//         console.log('counter sekarang = ' + counter)
+//         console.log('GANJIL')
+//     }
+// }
+
+// for(var counter = 1; counter <= 100; counter++){
+//     if (counter % 2 === 0) {
+//         console.log('counter sekarang = ' + counter)
+//         console.log('GENAP')
+//     } else {
+//         console.log('counter sekarang = ' + counter)
+//         console.log('GANJIL')
+//     }
+// }
+
+// for (var i = 1; i < 101; i += 2){
+//     if(0 == (i % 3)){
+//       console.log(i +' KELIPATAN 3');
+//     } else {
+//       console.log('');
+//     }
+// }
+
+//   for(var i =  1; i < 101; i += 5){
+
+//     if(0 == (i % 6)){
+//       console.log(i +' KELIPATAN 6');
+//     } else {
+//       console.log('');
+//     }
+//   }
+  for(var i =  1; i < 101; i += 9){
+  
+    if(0 == (i % 10)){
+      console.log(i +' KELIPATAN 10');
+    } else {
+      console.log('');
+    }
+  }
